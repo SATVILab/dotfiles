8b360b3ba5cce03f7bf31b1099a15266906127f0
Miguel Rodo <rdxmig002@myuct.ac.za>
2023-10-31 14:43:28 +0200

    Use config_r from CompTemplate

--- .bashrc.d/config_r.sh
+++ .bashrc.d/config_r.sh
@@ -6,17 +6,23 @@ if [ -n "$GH_TOKEN" ]; then
 fi
 
 # save all R packages to workspace if on GitPod
-if [ -n "$(env | grep -E "^GITPOD")" ]; then
-    R_LIBS=${R_LIBS:=/"/workspace/.local/lib/R"}
-    RENV_PATHS_CACHE=${RENV_PATHS_CACHE:=/workspace/.local/R/lib/renv}
-    RENV_PATHS_LIBRARY_ROOT=${RENV_PATHS_LIBRARY_ROOT:=/workspace/.local/.cache/R/renv}
-    RENV_PATHS_LIBRARY=${RENV_PATHS_LIBRARY:=/workspace/.local/.cache/R/renv}
-    RENV_PREFIX_AUTO=${RENV_PREFIX_AUTO:=TRUE}
+if [ -n "$(env | grep -E "^GITPOD|^CODESPACE")" ]; then
+    if [ -n "$(env | grep -E "^GITPOD")" ]; then
+      workspace_dir="/workspace"
+    else
+      workspace_dir="/workspaces"
+    fi
+    export R_LIBS=${R_LIBS:="$workspace_dir/.local/lib/R"}
+    export RENV_PATHS_CACHE=${RENV_PATHS_CACHE:=/"$workspace_dir/.local/R/lib/renv"}
+    export RENV_PATHS_LIBRARY_ROOT=${RENV_PATHS_LIBRARY_ROOT:=/"$workspace_dir/.local/.cache/R/renv"}
+    export RENV_PATHS_LIBRARY=${RENV_PATHS_LIBRARY:=/"$workspace_dir/.local/.cache/R/renv"}
+    export RENV_PREFIX_AUTO=${RENV_PREFIX_AUTO:=TRUE}
+    export RENV_CONFIG_PAK_ENABLED=${RENV_CONFIG_PAK_ENABLED:=TRUE}
 fi
 
-# ensure that radian works
+# ensure that radian works (at least on ephemeral dev
+# environments)
 if [ -n "$(env | grep -E "^GITPOD|^CODESPACE")" ]; then
-  export RENV_CONFIG_PAK_ENABLED=${RENV_CONFIG_PAK_ENABLED:=TRUE}
   if ! [ -e "$HOME/.radian_profile" ]; then touch "$HOME/.radian_profile"; fi
   if [ -z "$(cat "$HOME/.radian_profile" | grep -E 'options\(\s*radian\.editing_mode')" ]; then 
     echo 'options(radian.editing_mode = "vi")' >> "$HOME/.radian_profile"
