066c38fe9cbbfadd19cb55f65e3f93f9c519a9f4
Miguel Rodo <rdxmig002@myuct.ac.za>
2024-02-20 12:38:14 +0200

    Add sudo where rquird

--- scripts/install-jetbrains-font.sh
+++ scripts/install-jetbrains-font.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
 # Install fontconfig, wget and unzip
-apt-get update && apt-get install -y fontconfig wget unzip jq
+sudo apt-get update && sudo apt-get install -y fontconfig wget unzip jq
 
 # Create a directory for the font
-mkdir -p /tmp/jetbrains-mono
+sudo mkdir -p /tmp/jetbrains-mono
 
 # Download JetBrains Mono font
-wget -P /tmp/jetbrains-mono https://download.jetbrains.com/fonts/JetBrainsMono-2.242.zip
+sudo wget -P /tmp/jetbrains-mono https://download.jetbrains.com/fonts/JetBrainsMono-2.242.zip
 
 # Unzip the downloaded file
-unzip /tmp/jetbrains-mono/JetBrainsMono-2.242.zip -d /tmp/jetbrains-mono
+sudo unzip /tmp/jetbrains-mono/JetBrainsMono-2.242.zip -d /tmp/jetbrains-mono
 
 # Install the font
-mkdir -p /usr/share/fonts/truetype/jetbrains
-mv /tmp/jetbrains-mono/fonts/ttf/*.ttf /usr/share/fonts/truetype/jetbrains
-fc-cache -f -v
+sudo mkdir -p /usr/share/fonts/truetype/jetbrains
+sudo mv /tmp/jetbrains-mono/fonts/ttf/*.ttf /usr/share/fonts/truetype/jetbrains
+sudo fc-cache -f -v
 
 # Clean up
 cd ..
-rm -rf /tmp/jetbrains-mono
+sudo rm -rf /tmp/jetbrains-mono
 
 # Path to your settings.json file
 SETTINGS_PATH="$HOME/.vscode-remote/data/Machine/settings.json"
 
 # Check if the settings file exists and create it if it doesn't
 if [ ! -f "$SETTINGS_PATH" ]; then
-    echo "{}" > "$SETTINGS_PATH"
+    echo "{}" | sudo tee "$SETTINGS_PATH"
 fi
 
 # Update the settings
@@ -37,4 +37,4 @@ jq '. + {
     "terminal.integrated.fontFamily": "JetBrains Mono",
     "terminal.integrated.fontSize": 15,
     "terminal.integrated.lineHeight": 1.2
-}' "$SETTINGS_PATH" > temp.$$.json && mv temp.$$.json "$SETTINGS_PATH"
+}' "$SETTINGS_PATH" | sudo tee temp.$$.json && sudo mv temp.$$.json "$SETTINGS_PATH"
