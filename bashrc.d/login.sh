1d494c5cccc90e842651b5556cd83bf330d77da9
Miguel Rodo <miguel.rodo@uct.ac.za>
2024-08-20 10:33:34 +0000

    Add login.sh to bashrc.d

---
+++ bashrc.d/login.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export GITHUB_USERNAME=
+export GITHUB_USER=
+export GITHUB_PAT=
+export GH_TOKEN=$GITHUB_PAT
+export XETHUB_TOKEN=
+export XETHUB_PAT=
+export XETHUB_USERNAME=
+export XETHUB_EMAIL=
