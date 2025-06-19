b9df3411272275a6bcb75f10b302496256d9c04e
Miguel Rodo <rdxmig002@myuct.ac.za>
2025-06-19 11:23:37 +0200

    Correct syntax

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-# install.sh
-# This script sets up a development environment by configuring Bash, cloning a GitHub repository,
-# adding custom scripts to the user's PATH, and preparing the environment specifically for GitHub Codespaces.
-# It also ensures that .bashrc sources all files in .bashrc.d.
-
-install-env.sh codespace
+dotfiles_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+"$dotfiles_dir/install-env.sh" codespace
