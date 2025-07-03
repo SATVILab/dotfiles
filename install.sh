a877d3926613768a4ab78d15d65843e90bfe36b2
Miguel Rodo <rdxmig002@myuct.ac.za>
2025-07-03 12:07:01 +0200

    Use codespaces install settings only when $CODESPACES is set

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
 dotfiles_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-"$dotfiles_dir/install-env.sh" codespace
+if [ "$CODESPACES" = "true" ]; then
+  echo "Running in Codespaces environment"
+  "$dotfiles_dir/install-env.sh" codespace
+else
+  echo "Not running in Codespaces environment"
+  "$dotfiles_dir/install-env.sh" dev
+fi
+
